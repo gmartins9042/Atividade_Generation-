a24bef297a69661fd71c7c86ea6e29d672493588
gmartins9042 <gmartins9042@gmail.com>
2025-06-30 20:39:27 -0300

    Foi adionado folha de pagamento

--- Atividade1/questao1.js
+++ Atividade1/questao1.js
@@ -7,9 +7,14 @@ let salario = readline.questionFloat("Digite seu Salario: ");
 let abono = readline.questionFloat("Digite seu abono: ");
 let novoSalario = (salario + abono );
 
+//Lmpa
+console.clear();
 
 //Saida
-console.log(`Total a receber é de: ${novoSalario.toFixed(2)}`);
+console.log(`*******PAGAMENTO*******`);
+console.log(`Sálario : R$ ${salario.toFixed(2)}`);
+console.log(`Abono   : R$ ${abono.toFixed(2)}`);
+console.log(`Total   : R$ ${novoSalario.toFixed(2)}`);
 
 
 
