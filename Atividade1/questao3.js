6d18929b8ae5a79766269188c15117d16b480a8c
gmartins9042 <gmartins9042@gmail.com>
2025-06-30 20:25:35 -0300

    Foi adicionado um limpa tela e organizado como folha de pagamento

--- Atividade1/questao3.js
+++ Atividade1/questao3.js
@@ -2,13 +2,20 @@
 const readline = require('readline-sync');
 
 //Entrada
-
-let salarioBruto = readline.questionFloat("Digite seu Salário Bruto: ");
+let salarioBruto = readline.questionFloat("Digite seu Salario Bruto: ");
 let adicionalNoturno = readline.questionFloat("Digite seu Adicional Noturno: ");
 let horaExtras = readline.questionFloat("Digite suas Horas Extras: ");
 let descontos = readline.questionFloat("Digite o Descontos: ");
 let salarioLiquido = salarioBruto + adicionalNoturno + (horaExtras * 5) - descontos;
 
-//Saida 
+//Limpa
+console.clear();
 
-console.log(`Seu salário liquido é de ${salarioLiquido.toFixed(2)}`);
+//Saida 
+console.log(`======= FOLHA DE PAGAMENTO =======`);
+console.log(`Salário Bruto      : R$ ${salarioBruto.toFixed(2)}`);
+console.log(`Adicional Noturno  : R$ ${adicionalNoturno.toFixed(2)}`);
+console.log(`Horas Extras       : ${horaExtras} x R$5.00`);
+console.log(`Descontos          : R$ ${descontos.toFixed(2)}`);
+console.log(`-----------------------------------`);
+console.log(` Salário Líquido   : R$ ${salarioLiquido.toFixed(2)}`);
