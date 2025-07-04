174d36ea5383ea534edd38777e707287c94e0706
gmartins9042 <gmartins9042@gmail.com>
2025-07-03 22:43:05 -0300

    Atualizando

--- atividade4/exercicio2.ts
+++ atividade4/exercicio2.ts
@@ -1,3 +1,15 @@
 //Importar o readline
 import readLine = require("readline-sync");
 
+//Lista
+const list: Set<number> = new Set<number>();
+
+//Pedindo ao usuario que digite numero de 1 a 10.
+console.log("Digite de 1 a 10 Aleatoriamente.");
+
+for (let i = 1; i <= 10; i++) {
+    const numero = readLine.questionInt("Digite o numero: ");
+    list.add(numero);
+}
+console.table("Listar dados do Set");
+list.forEach((n)=> console.log(n));
