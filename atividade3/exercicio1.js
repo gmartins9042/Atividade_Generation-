4886ccf43947446e47b54767a8f839fad7a78f8c
gmartins9042 <gmartins9042@gmail.com>
2025-07-07 19:36:45 -0300

    Atualizando a linha 6 a linha 10, adicionando o intervalo invalido

--- atividade3/exercicio1.js
+++ atividade3/exercicio1.js
@@ -3,10 +3,15 @@ const readline = require('readline-sync');
 // Entrada
 let numero1 = readline.questionInt("Digite o inicio do intervalo: ");
 let numero2 = readline.questionInt("Digite o fim do intervalo: ");
+if (numero1 > numero2) {
 
-// Processamento e saída
-for (let i = numero1; i <= numero2; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`${i} é multiplo de 3 e 5`);
+  console.log("Intervalo inválido: o número inicial deve ser menor ou igual ao número final.");
+
+} else {
+  // Processamento e saída
+  for (let i = numero1; i <= numero2; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`${i} é multiplo de 3 e 5`);
+    }
   }
 }
